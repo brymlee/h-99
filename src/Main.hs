150bbcc599253f91f8175272facd880255a3d0b4
brymlee <brymlee1911@gmail.com>
2022-09-16 11:19:03 -0600

    Problem 3

--- src/Main.hs
+++ src/Main.hs
@@ -2,23 +2,35 @@
 module Main where
 
 import Prelude ( IO
+               , Integer
                , putStrLn
                , reverse
                , head
                , foldl
                , show
                , map
+               , filter
+               , zip
                , return
                , tail
                , (.)
                , ($)
-               , (<>))
+               , (<>)
+               , (==))
+import Data.List (genericLength)
+import Data.Tuple (fst, snd)
 
 myLast :: [a] -> a
 myLast = head . reverse
 
 myButLast :: [a] -> a
 myButLast = head . tail . reverse
+
+elementAt :: [a] -> Integer -> a
+elementAt xs a = head $
+  map snd $ 
+    filter (((==) a). fst) $ 
+      zip [1 .. genericLength xs] xs
 
 main :: IO ()
 main = do
@@ -31,4 +43,8 @@ main = do
       , (<>) "Problem 2 - myButLast [1, 2, 3, 4]: " $
           show $ myButLast [1, 2, 3, 4]
       , (<>) "Problem 2 - myButLast ['a' .. 'z']: " $
-          show $ myButLast ['a' .. 'z']]
+          show $ myButLast ['a' .. 'z']
+      , (<>) "Problem 3 - elementAt [1, 2, 3] 2: " $
+          show $ elementAt [1, 2, 3] 2
+      , (<>) "Problem 3 - elementAt \"haskell\" 5: " $
+          show $ elementAt "haskell" 5]
